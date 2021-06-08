7a807d77698071c1a9eff770bccb7a2632d17f74
asmbd <asmb.dwr@gmail.com>
2021-06-08 21:57:47 +0700

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by asmb on 8/6/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

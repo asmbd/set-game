c90df6deb0d5ea483bacfa71a48c678e9a0fd934
asmbd <asmb.dwr@gmail.com>
2021-06-09 00:18:32 +0700

    [+] add: setup start cards

--- Set/ContentView.swift
+++ Set/ContentView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    var game = SetGame()
+    var cardPerRow = 4
+    
+    @State var selectedCards = [UUID]()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(
+            alignment: .center,
+            spacing: 16.0
+        ) {
+            ForEach(0..<game.cards.count/cardPerRow) { row in
+                HStack(
+                    alignment: .center,
+                    spacing: 16.0
+                ) {
+                    ForEach(0..<cardPerRow) { index in
+                        let cardIndex = (Int(row) * cardPerRow) + index
+                        let card = game.cards[cardIndex]
+                        Button("\((Int(row) * cardPerRow) + index)") {
+                            selectedCards.append(card.id)
+                            print("\(game.cards[index])")
+                        }
+                        .frame(width: 80.0, height: 120.0)
+                        .border(selectedCards.contains(card.id) ? Color.red : Color.black, width: 2)
+                    }
+                }
+            }
+            VStack {
+                Button("Deal Cards") {
+                    print("deal")
+                }
+            }
+        }
     }
 }
 
